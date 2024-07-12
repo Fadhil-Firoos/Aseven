c0e7ab9ae566a852d7dd931a652112db570186f1
ferdinan <verdzl126@gmail.com>
2024-07-13 02:09:02 +0700

    Update development fix logout controller

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome')->name('home');
 
 Route::middleware('guest')->group(function () {
-    Route::get('login', Login::class)
-        ->name('login');
+    Route::get('/', Login::class)
+    ->name('login');
 
     Route::get('register', Register::class)
         ->name('register');
